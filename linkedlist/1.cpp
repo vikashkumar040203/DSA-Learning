da2d280d35c36f38dc96f8be76b61fed328720c4
vikashkumar040203 <vikashkumar990523@gmail.com>
2022-06-08 13:48:50 +0530

    linkedlist

--- linkedlist/1.cpp
+++ linkedlist/1.cpp
@@ -63,32 +63,82 @@ void display(node *head)
     }
     cout << "NULL" << endl;
 }
-void deletion(node *&head, int val)
-{
-    if (head == NULL)
-        return;
-    node *temp = head;
-    while (temp->next->data != val)
-    {
-        temp = temp->next;
-    }
-    if(head->next==NULL){
-        deleteatFirst(head);
-    }
-    node *todelete = temp->next;
-    temp->next = temp->next->next;
-    delete todelete;
-}
+// void deletion(node *&head, int val)
+// {
+//     if (head == NULL)
+//         return;
+//     node *temp = head;
+//     while (temp->next->data != val)
+//     {
+//         temp = temp->next;
+//     }
+//     if(head->next==NULL){
+//         deleteatFirst(head);
+//     }
+//     node *todelete = temp->next;
+//     temp->next = temp->next->next;
+//     delete todelete;
+// }
 // delete at head
-void deleteatFirst(node *&head)
-{
-    node *todelete = head;
-    head = head->next;
-    delete todelete;
-}
-
+// void deleteatFirst(node *&head)
+// {
+//     node *todelete = head;
+//     head = head->next;
+//     delete todelete;
+// }
 
 // reverse LinkedList
+
+// node* reverse(node* &head){
+//     node* prev= NULL;
+//     node* current = head;
+//     node* nextptr;
+//     while(current!=NULL){
+//         nextptr= current->next;
+//         current->next=prev;
+//         prev=current;
+//         current=nextptr;
+//     }
+//     return prev;
+// }
+
+// or using recursin
+
+// node* reverserecursive(node* &head){
+//     if(head==NULL || head->next==NULL) return head;
+
+//     node* newhead = reverserecursive(head->next);
+//     head->next->next=head;
+//     head->next=NULL;
+//     return newhead;
+
+// }
+// node *reverseK(node *&head, int k)
+// {
+
+//     node *prev = NULL;
+//     node *curr = head;
+//     node *nextptr;
+//     int count = 0;
+//     while (curr != NULL && count < k)
+//     {
+//         nextptr = curr->next;
+//         curr->next = prev;
+
+//         prev = curr;
+//         curr = nextptr;
+//         count++;
+//     }
+//     if (nextptr != NULL)
+//     {
+
+//         head->next = reverseK(nextptr, k);
+//     }
+//     return prev;
+// }
+
+
+// Detection Of cycle and removal 
 
 
 
@@ -100,6 +150,10 @@ int main()
     insertAtBegining(head, 6);
     insertAtBegining(head, 12);
     insertAtBegining(head, 62);
+    display(head);
+    // node *newhead = reverserecursive(head);
+
+    // display(newhead);
 
     return 0;
 }
