8f0a7679fbb91a461ac50dc62700959bc98de1d1
vikashkumar040203 <vikashkumar990523@gmail.com>
2022-06-09 21:25:51 +0530

    doubly-linkedlist

--- linkedlist/1.cpp
+++ linkedlist/1.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
 using namespace std;
 // defining node
-class node
-{
-public:
-    int data;
-    node *next;
-    node(int val)
-    {
-        data = val;
-        next = NULL;
-    }
-};
+// class node
+// {
+// public:
+//     int data;
+//     node *next;
+//     node(int val)
+//     {
+//         data = val;
+//         next = NULL;
+//     }
+// };
 
 // void insertAtEnd(node *&head, int val)
 // {
@@ -31,38 +31,38 @@ public:
 // }
 
 // insertion at the beginig
-void insertAtBegining(node *&head, int val)
-{
+// void insertAtBegining(node *&head, int val)
+// {
 
-    node *n = new node(val);
-    n->next = head;
-    head = n;
-}
+//     node *n = new node(val);
+//     n->next = head;
+//     head = n;
+// }
 
-bool search(node *head, int key)
-{
-    node *temp = head;
-    while (temp != NULL)
-    {
-        if (temp->data = key)
-        {
-            return true;
-        }
-        temp = temp->next;
-    }
-    return false;
-}
+// bool search(node *head, int key)
+// {
+//     node *temp = head;
+//     while (temp != NULL)
+//     {
+//         if (temp->data = key)
+//         {
+//             return true;
+//         }
+//         temp = temp->next;
+//     }
+//     return false;
+// }
 
-void display(node *head)
-{
-    node *temp = head;
-    while (temp != NULL)
-    {
-        cout << temp->data << "->";
-        temp = temp->next;
-    }
-    cout << "NULL" << endl;
-}
+// void display(node *head)
+// {
+//     node *temp = head;
+//     while (temp != NULL)
+//     {
+//         cout << temp->data << "->";
+//         temp = temp->next;
+//     }
+//     cout << "NULL" << endl;
+// }
 // void deletion(node *&head, int val)
 // {
 //     if (head == NULL)
@@ -137,23 +137,206 @@ void display(node *head)
 //     return prev;
 // }
 
+// Detection Of cycle and removal
+// bool detectCycle(node *head)
+// {
+//     node *fast = head;
+//     node *slow = head;
+//     while (fast != NULL && fast->next != NULL)
+//     {
+//         slow = slow->next;
+//         fast = fast->next->next;
+//         if (fast = slow)
+//         {
+//             return 1;
+//         }
+//         else
+//             return 0;
+//     }
+// }
 
-// Detection Of cycle and removal 
+// make cycle
 
+// void makecycle(node *&head, int pos)
+// {
+//     node *temp = head;
+//     node *startNode;
+//     int count = 1;
+//     while (temp->next != NULL)
+//     {
+//         if (count == pos)
+//         {
+//             startNode = temp;
+//         }
+//         temp = temp->next;
+//         count++;
+//     }
+//     temp->next = startNode;
+// }
+// // remove cycle
 
+// void removecycle(node *&head)
+// {
+//     node *fast = head;
+//     node *slow = head;
+//     do
+//     {
+//         /* code */
+//         slow = slow->next;
+//         fast = fast->next->next;
+//     } while (slow != fast);
+//     fast = head;
+//     while (slow->next != fast->next)
+//     {
+//         slow = slow->next;
+//         fast = fast->next;
+//     }
+//     slow->next = NULL;
+// }
 
+// Doubly LinkedList
+
+class node
+{
+public:
+    int data;
+    node *next;
+    node *prev;
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+        prev = NULL;
+    }
+};
+
+// insert at  tail
+void insertatHead(node *&head, int val)
+{
+
+    node *n = new node(val);
+    n->next = head;
+    if (head != NULL)
+    {
+
+        head->prev = n;
+        head = n;
+    }
+}
+void insertattail(node *&head, int val)
+{
+    if (head == NULL)
+    {
+        insertatHead(head, val);
+        return;
+    }
+    node *n = new node(val);
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->prev = temp;
+}
+// Insert at head
+
+// display of doubly linkedList
+
+// deletion
+void deleteAtHead(node *head)
+{
+    node *todelete = head;
+    head = head->next;
+    head->prev = NULL;
+    delete todelete;
+}
+void deletion(node *&head, int pos)
+{
+
+    if (pos == 1)
+    {
+        deleteAtHead(head);
+    }
+
+    node *temp = head;
+    int count = 1;
+    while (temp != NULL && count != pos)
+    {
+        temp = temp->next;
+        count++;
+    }
+    temp->prev->next = temp->next;
+    if (temp->next != NULL)
+    {
+
+        temp->next->prev = temp->prev;
+    }
+    delete temp;
+}
+
+void display(node *head)
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+// apppend last k nodes  to the start of link list
+int length(node *head)
+{
+    int l = 0;
+    node *temp = head;
+    while (temp != NULL)
+    {
+        l++;
+        temp = temp->next;
+    }
+    return l;
+}
+
+node *Kappend(node *&head, int k)
+{
+    node *newHead;
+    node *newtail;
+    node *tail = head;
+    int l = length(head);
+    k = k % l;
+    int count = 1;
+    while (tail->next != NULL)
+    {
+        if (count == l - k)
+        {
+            newtail = tail;
+        }
+        if (count == l - k + 1)
+        {
+            newHead = tail;
+        }
+        tail = tail->next;
+        count++;
+    }
+    newtail->next = NULL;
+    tail->next = head;
+    return newHead;
+}
 int main()
 
 {
-    node *head = NULL;
-    insertAtBegining(head, 1);
-    insertAtBegining(head, 6);
-    insertAtBegining(head, 12);
-    insertAtBegining(head, 62);
-    display(head);
-    // node *newhead = reverserecursive(head);
 
-    // display(newhead);
+    node *head = NULL;
+    insertatHead(head, 5);
+    insertatHead(head, 4);
+    insertatHead(head, 3);
+    insertatHead(head, 2);
+    insertatHead(head, 1);
+    display(head);
+    insertatHead(head, 6);
+    display(head);
 
     return 0;
 }
